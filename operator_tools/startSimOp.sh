91cf378c7f5ca7428b11fdfdb5bf5183a67cabbf
jjchromik <justyna.chromik@gmail.com>
2018-07-18 15:58:20 +0200

    Removed the operator tools

--- operator_tools/startSimOp.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source ~/.virtualenvs/mosaik_new/bin/activate
-
-cd ..
-
-python demo_vuln.py 127.0.0.1:8000
--- topology_loader/startSimTopo.sh
+++ topology_loader/startSimTopo.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+# cd ..
+# cd operator_tools
+# python operator_gui.py
+
+source ~/.virtualenvs/mosaik_new/bin/activate
+
 cd ..
-cd operator_tools
-python operator_gui.py
+
+python demo_vuln.py 127.0.0.1:8000
